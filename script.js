12cf51a90b2fa1a921681f21e8b278c17bb0a17f
Rija2005 <164767074+Rija2005@users.noreply.github.com>
2024-05-22 15:59:20 +0500

    my practice

---
+++ script.js
@@ -0,0 +1,81 @@
+//assignment
+
+//queston1
+
+// let city =+prompt ("Enter City");
+// if ( city==="karachi"){
+//     console.log("welcome to city of lights.");
+// }
+// else{
+//     console.log("Welcome to another world");
+// }
+// //question2
+// let gender =prompt("Enter your Gender");
+// if(gender==="male"){
+//     console.log("Good Morning Sir");
+// }
+// else if ( gender ==="female"){
+//     console.log("Good Morning Ma'am");
+// }
+// else{
+//     console.log("other");
+// }
+// //question3
+
+// let signals = prompt ("Enter Signal");
+// if (signals==="red"){
+//     alert("must stop")
+// }
+// else if(signals==="yellow"){
+//     alert("Ready to Move")
+// }
+// else if (signals==="green"){
+//     alert("Move now")
+// }
+// else{
+//     console.log("On the Way");
+// }
+//chapter 15
+//arrays
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+
+// let cities =["Atlanta", "Baltimore","Chicago","Denver", "Los Angeles","Seattle"]
+// console.log(cities.length);
+// console.log(cities[1].length);
+
+
+// let number=[ 1,2,3,4,5,6,7,8,9];
+// console.log(number);
+
+// let info =["rija",18 , "wasifa" ,21 ,"eshal" ,10];
+// console.log(info[1].length);
+
+
+// //mixed array
+
+// let mixedArray =[ 1, "boby" , true ,"nowis"];
+// console.log(mixedArray[0].length);//undefined
+// console.log(mixedArray[1].length);
+// console.log(mixedArray[2].length);//undefined
+// console.log(mixedArray[3].length);
+ 
+//arrays method
+//push()start from end
+//pop()dlete from end n return
+//tosring()
+
+
+let cities =["Atlanta", "Baltimore","Chicago","Denver", "Los Angeles","Seattle"]
+console.log(cities.push("pakistan","India"));
+// console.log(cities.pop("india"));
+console.log(cities.toString());
+console.log(cities);
+
+
